cd622f08ae0c084a238125d094c8f07bd71e2fc5
Josh Pollock <jpollock412@gmail.com>
2021-08-25 17:09:59 -0400

    add og:image from Beyond Code

--- src/components/HtmlHead.js
+++ src/components/HtmlHead.js
@@ -1,7 +1,16 @@
 import Head from 'next/head'
 import twitterCard from '@/img/twitter-card.jpg'
 
+const makeBannerSrc = ({ title, description, theme, images }) =>
+  `https://banners.beyondco.de/${encodeURI(
+    title
+  )}.png?theme=${theme}&pattern=bamboo&style=style_1&description=${encodeURI(
+    description
+  )}&md=1&showWatermark=0&fontSize=100px&images=${
+    images ? images : 'terminal'
+  }&widths=250&heights=100&packageName=+Josh+Pollock+|+JoshPress.net+`
 const HtmlHead = ({ title, description, children, image }) => {
+  const imgSrc = image ? image : makeBannerSrc({ title, description, theme: 'dark' })
   const pageTitle = `${title ? `${title} | Josh Pollock` : 'Josh Pollock'}`
   const pageDescription = description
     ? description
@@ -14,15 +23,12 @@ const HtmlHead = ({ title, description, children, image }) => {
         <meta name="twitter:creator" content="@josh412" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={pageDescription} />
-        <meta
-          name="twitter:image"
-          content={image ? image : `https://joshpress.net${twitterCard}`}
-        />
+        <meta name="twitter:image" content={imgSrc} />
         <meta property="og:url" content="https://joshpress.net" />
         <meta property="og:type" content="article" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content={image ? image : `https://joshpress.net${twitterCard}`} />
+        <meta property="og:image" content={imgSrc} />
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
         {children && { children }}
